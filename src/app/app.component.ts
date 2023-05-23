a13fe3652c3ed7cef97f53f32fd5a63c36bfebab
Felix Egli <felix@crossequip.ch>
2023-05-23 19:34:23 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,29 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+export interface Pizza {
+  name: string;
+  price: number;
+}
+
+export interface OrderItem {
+  amount: number;
+  pizza: Pizza;
+}
+
+export class Order {
+  public orderItems: OrderItem[] = [];
+  email: string = '';
+
+  get orderTotal(): number {
+    let total = 0;
+    this.orderItems.forEach(orderItem => {
+      total += orderItem.amount * orderItem.pizza.price;
+    });
+    return total;
+  }
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +34,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'pizza-order2';
+  order: Order = new Order();
+
+  constructor() {
+    this.order.email = 'meine@email.com';
+    this.order.orderItems = [
+      { amount: 1, pizza: { name: 'Margaritha', price: 12 } },
+      { amount: 2, pizza: { name: 'Prosciutto', price: 16 } },
+      { amount: 3, pizza: { name: 'Hawaii', price: 18 } },
+    ];
+  }
+
 }
