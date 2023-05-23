c1679673abad11fe488a2a57517ba4e0e51c0cd9
Felix Egli <felix@crossequip.ch>
2023-05-23 19:38:31 +0200

    added ng model from FormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 export interface Pizza {
   name: string;
@@ -28,7 +29,7 @@ export class Order {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
