194bafaf3fe02e96aa58f423b421e9eed42610dd
Felix Egli <felix@crossequip.ch>
2023-05-23 20:26:51 +0200

    add order item

---
+++ src/app/order-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'order-item',
+  templateUrl: 'order-item.component.html'
+})
+
+export class OrderItemComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
