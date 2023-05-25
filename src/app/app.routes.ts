cffea85747f93f2a0bea6c22b6ab89f5211967bb
Felix Egli <felix@crossequip.ch>
2023-05-25 20:28:43 +0200

    added default and wildcard routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,7 @@ import { OrderComponent } from './order.component';
 
 export const routes: Routes = [
   { path: 'order', component: OrderComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/order', pathMatch: 'full'},
+  { path: '**', component: OrderComponent}, // 404 Component oder Redirect
 ];
